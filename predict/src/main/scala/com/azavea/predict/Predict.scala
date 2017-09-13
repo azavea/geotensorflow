44b4f7169780cd0de02229e32b53c5f1ad4eebd7
yoninachmany <yoninachmany@gmail.com>
2017-09-13 15:47:41 -0400

    following dir structure of multiproject, Predict.scala is Main.scala from geotrellis-sbt-template; build.sbt from pointcloud ingest/build.sbt, minus pointcloud and ficus

---
+++ predict/src/main/scala/com/azavea/predict/Predict.scala
@@ -0,0 +1,12 @@
+package predict
+
+import geotrellis.raster._
+import geotrellis.spark._
+
+object Predict {
+  def helloSentence = "Hello GeoTrellis"
+
+  def main(args: Array[String]): Unit = {
+    println(helloSentence)
+  }
+}
